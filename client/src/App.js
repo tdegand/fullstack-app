b71cc85ff2b5a8e6c09eaa338600456506376963
Tyler Degand <tylerdegand@gmail.com>
2020-09-23 11:54:23 -0500

    update some items

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React, {Component, useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import CourseDetail from './components/courseDetail'
 import './global.css';
 import Header from './components/home';
 import Courses from './components/courses';
@@ -7,13 +13,21 @@ class App extends Component{
 
   render() {
     return (
-    <div id="root">
-      <div>
-        <Header />
-        <hr />
-        <Courses />
-      </div>
-    </div>
+      <Router>
+        <div id="root">
+          <div>
+            <Header />
+            <Switch>
+              <Route path="/">
+                <Courses />
+              </Route>
+              <Route path="/courses/:id">
+                <CourseDetail />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/course.js
+++ client/src/components/course.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Course = (props) => {
     return (
         <div className="grid-33">
-            <a className="course--module course--link" href="course-detail.html">
+            <a className="course--module course--link" href={`/courses/${props.course.id}`}>
               <h4 className="course--label">Course</h4>
               <h3 className="course--title">{props.course.title}</h3>
             </a>
