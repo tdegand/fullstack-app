5d179a98b51f5f4046b8738fcca0eb9766f01c1b
Tyler Degand <tylerdegand@gmail.com>
2020-09-30 13:39:59 -0500

    added placeholder information and structure

--- client/src/components/userSignOut.js
+++ client/src/components/userSignOut.js
@@ -3,7 +3,7 @@ import React from 'react'
 const UserSignOut = () => {
 
     return(
-
+        <h1>Placeholder</h1>
     );
 }
 
--- client/src/components/userSignUp.js
+++ client/src/components/userSignUp.js
@@ -3,7 +3,36 @@ import React from 'react'
 const UserSignUp = () => {
 
     return(
-
+        <div class="bounds">
+            <div class="grid-33 centered signin">
+            <h1>Sign Up</h1>
+            <div>
+                <form>
+                    <div>
+                        <input id="firstName" name="firstName" type="text" class="" placeholder="First Name" value="" />
+                    </div>
+                    <div>
+                        <input id="lastName" name="lastName" type="text" class="" placeholder="Last Name" value="" />
+                    </div>
+                    <div>
+                        <input id="emailAddress" name="emailAddress" type="text" class="" placeholder="Email Address" value="" />
+                    </div>
+                    <div>
+                        <input id="password" name="password" type="password" class="" placeholder="Password" value="" />
+                    </div>
+                    <div>
+                    <input id="confirmPassword" name="confirmPassword" type="password" class="" placeholder="Confirm Password"value="" />
+                    </div>
+                < div class="grid-100 pad-bottom">
+                        <button class="button" type="submit">Sign Up</button>
+                        <button class="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    </div>
+                </form>
+            </div>
+            <p>&nbsp;</p>
+            <p>Already have a user account? <a href="sign-in.html">Click here</a> to sign in!</p>
+            </div>
+        </div>
     );
 }
 
