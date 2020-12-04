44fd9b0ea474b3a109dd63e39c04e35b4c48fcd2
Tyler Degand <tylerdegand@gmail.com>
2020-12-04 11:38:14 -0600

    added comments to come back to later

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Header from "./components/header";
 import Courses from "./components/courses";
 import Coursedetail from "./components/courseDetail";
@@ -7,35 +7,45 @@ import UpdateCourse from "./components/updateCourse";
 import UserSignIn from "./components/userSignIn";
 import UserSignUp from "./components/userSignUp";
 import UserSignOut from "./components/userSignOut";
-import withContext from "./context";
 import "./global.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Axios from "axios";
 
-const UserSignUpWithContext = withContext(UserSignUp);
-const UserSignInWithContext = withContext(UserSignIn);
-const UserSignOutWithContext = withContext(UserSignOut);
+const App = () => {
 
-class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div id="root">
-					<div>
-						<Header />
-						<Switch>
-							<Route exact path="/" component={Courses}></Route>
-              <Route path="/courses/create" component={CreateCourse}></Route>
-							<Route path="/courses/:id"component={Coursedetail}></Route>
-							<Route path="/courses/:id/update" component={UpdateCourse}></Route>
-							<Route path="/signin" component={UserSignInWithContext}></Route>
-							<Route path="/signup" component={UserSignUpWithContext}></Route>
-							<Route path="/signout" component={UserSignOutWithContext}></Route>
-						</Switch>
-					</div>
+  const [values, setValues] = useState({
+    //use stored authenticated user from cookie or set it to null
+		authenticatedUser: ""
+	});
+
+  //signIn method is going to need some work here to authenticate and retrun the ser and then store it in a cookie
+	const signIn = (emailAddress, password) => {
+    Axios.get('http://localhost:5000/api/users')
+    .then(res => {
+      console.log(res)
+      console.log(res.data)
+    })
+  };
+  signIn("tylerdegand@gmail.com", "password")
+
+	return (
+		<Router>
+			<div id="root">
+				<div>
+					<Header />
+					<Switch>
+						<Route exact path="/" component={Courses}></Route>
+						<Route path="/courses/create" component={CreateCourse}></Route>
+						<Route path="/courses/:id" component={Coursedetail}></Route>
+						<Route path="/courses/:id/update" component={UpdateCourse}></Route>
+						<Route path="/signin" component={UserSignIn}></Route>
+						<Route path="/signup" component={UserSignUp}></Route>
+						<Route path="/signout" component={UserSignOut}></Route>
+					</Switch>
 				</div>
-			</Router>
-		);
-	}
-}
+			</div>
+		</Router>
+	);
+};
 
 export default App;
